722a61eac49e816583adefbd5983badeb03935cd
DINESH-JUMANI <jumanidinesh981@gmail.com>
2024-12-05 13:22:49 +0530

    Integrated the category Api With redux

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,9 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import categoryReducer from './slice/categorySlice';
 import cartSlice from "../Features/Cart/cartSlice";
 import wishListSlice from "../Features/Wishlist/wishListSlice";
 
+
 const store = configureStore({
   reducer: {
+    category: categoryReducer,
     cart: cartSlice,
     wishlist: wishListSlice,
   },
