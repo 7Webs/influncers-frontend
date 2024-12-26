87d737796dd703b0ce9aa108b2fbf145be572391
tanay388 <tanay.deo388@gmail.com>
2024-12-26 12:28:11 +0530

    fix: page

--- src/Api/apiwrapper.js
+++ src/Api/apiwrapper.js
@@ -1,33 +1,43 @@
 import axios from "axios";
 import { getAuth } from "firebase/auth";
 import { app } from "../firebase";
+import { toast } from "react-toastify";
 
 const api = axios.create({
-    baseURL: " http://localhost:3000",
+  //   baseURL: " http://localhost:3000",
+  baseURL: "https://clownfish-app-4jmg2.ondigitalocean.app/",
 });
 
 const firebaseTokenInterceptor = async (config) => {
-    const auth = getAuth(app);
-    const user = auth.currentUser;
+  const auth = getAuth(app);
+  const user = auth.currentUser;
 
-    if (user) {
-        try {
-            const token = await user.getIdToken();
-            config.headers["Authorization"] = `Bearer ${token}`;
-        } catch (error) {
-            console.error("Error getting Firebase token:", error);
-        }
+  if (user) {
+    try {
+      const token = await user.getIdToken();
+      config.headers["Authorization"] = `Bearer ${token}`;
+    } catch (error) {
+      console.error("Error getting Firebase token:", error);
     }
+  }
 
-    return config;
+  return config;
 };
 
 api.interceptors.request.use(firebaseTokenInterceptor);
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    toast.error(error.response.data.message);
+    return Promise.reject(error);
+  }
+);
+
 export const apiService = {
-    get: (url, config) => api.get(url, config),
-    post: (url, data, config) => api.post(url, data, config),
-    put: (url, data, config) => api.put(url, data, config),
-    delete: (url, config) => api.delete(url, config),
-    patch: (url, data, config) => api.patch(url, data, config),
+  get: (url, config) => api.get(url, config),
+  post: (url, data, config) => api.post(url, data, config),
+  put: (url, data, config) => api.put(url, data, config),
+  delete: (url, config) => api.delete(url, config),
+  patch: (url, data, config) => api.patch(url, data, config),
 };
